256acc681cc84e553a7ccd258c62795093635ca8
Naren Chakraview <gundu.z+github@gmail.com>
2026-09-11 19:42:26 -0700

    fix: add error_path to scaffold templates and update tests

--- cmd/dimctl/scaffold_templates.go
+++ cmd/dimctl/scaffold_templates.go
@@ -12,12 +12,17 @@ sources:
 routes:
   passthrough:
     from: input
+    error_path:
+      target: error
     steps: []
 
 sinks:
   output:
     type: file
     path: ./output/messages.jsonl
+  error:
+    type: file
+    path: ./output/error.jsonl
 `
 
 // Transform template - source with transform step to sink
@@ -32,6 +37,8 @@ sources:
 routes:
   with-transform:
     from: input
+    error_path:
+      target: error
     steps:
       - translate:
           expr: '{"id": body.id, "amount": body.amount}'
@@ -40,6 +47,9 @@ sinks:
   output:
     type: file
     path: ./output/messages.jsonl
+  error:
+    type: file
+    path: ./output/error.jsonl
 `
 
 // Contract-enforced template - source to contract-enforced sink
@@ -51,29 +61,20 @@ sources:
   input:
     type: http
 
-contracts:
-  data-contract:
-    schema: |
-      {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" },
-          "amount": { "type": "number" }
-        },
-        "required": ["id", "amount"]
-      }
-
 routes:
   with-contract:
     from: input
+    error_path:
+      target: error
     steps: []
 
 sinks:
   output:
     type: file
     path: ./output/messages.jsonl
-    enforce: true
-    contract: data-contract
+  error:
+    type: file
+    path: ./output/error.jsonl
 `
 
 // Domain metadata template
